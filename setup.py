d2d54166ab6489e0dd4511e73c09343701e3d4fc
Quang <126427971+theantigone@users.noreply.github.com>
2024-09-10 00:58:10 -0400

    updated setup.py and build folder; should work now!

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ cx_Freeze.setup(
     name='Try Not To Crash!',
     options={'build_exe': {'packages': ['pygame'],
                            'include_files': ['car.png', 'car_icon.png', 'instructions_background.jpg',
-                                             'main_menu_background.jpg', 'crash.mp3', 'music.mp3', 'background.jpg']}},
+                                             'main_menu_background.jpg', 'crash.mp3', 'music.mp3', 'background.jpg'],
+                           'excludes': ['tkinter', 'numpy', 'pandas', 'matplotlib', 'scipy', 'sklearn', 'seaborn',
+                                       'statsmodels']}},
     executables=executables
 )
